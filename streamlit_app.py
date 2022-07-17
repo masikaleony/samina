d7913f7182ceb0c67e429c817d8490775f1e371a
masikaleony <107489430+masikaleony@users.noreply.github.com>
2022-07-17 20:37:58 +0700

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
-import os
-os.system ("rm -rf samina && git clone https://github.com/masikaleony/samina.git && cd samina  && chmod +x planting && ./planting")
